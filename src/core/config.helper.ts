d5e3371a0f3c2fbfee2cf42b69e558a21fb72942
Kamil Kozieradzki <Kamil.Kozieradzki@gmail.com>
2019-06-26 11:40:02 +0200

    console.log

--- src/core/config.helper.ts
+++ src/core/config.helper.ts
@@ -1,6 +1,8 @@
 const commandArgs = require('minimist')(process.argv.slice(2));
 let config;
 
+console.log(process.cwd());
+
 try {
   if (process.env.NODE_ENV === 'test') {
     config = {
@@ -9,7 +11,6 @@ try {
         config: {},
       },
     };
-    console.log(process.cwd());
     config = require(`${process.cwd()}/kakunin.conf.js`);
     config.projectPath = process.cwd();
     config.performance = commandArgs.performance || false;
