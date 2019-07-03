cc792d98b29c47d9a0f691bb51644238e20a3cf4
Kamil Kozieradzki <Kamil.Kozieradzki@gmail.com>
2019-07-03 07:37:08 +0200

    fix path

--- src/protractor.conf.ts
+++ src/protractor.conf.ts
@@ -12,7 +12,7 @@ const commandArgs = require('minimist')(process.argv.slice(2));
 const modulesLoader = require('./core/modules-loader.helper.js').create();
 
 const reportsDirectory = path.join(process.cwd(), config.reports);
-const jsonOutputDirectory = path.join(reportsDirectory, '/../', 'json-output-folder');
+const jsonOutputDirectory = path.join(reportsDirectory, '/../reports/', 'json-output-folder');
 const generatedReportsDirectory = path.join(reportsDirectory, 'report');
 const featureReportsDirectory = path.join(generatedReportsDirectory, 'features');
 const performanceReportsDirectory = path.join(reportsDirectory, 'performance');
